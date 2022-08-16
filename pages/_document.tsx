f7446d927e5193222445b8a769f652240b0034e1
haZya <63403456+haZya@users.noreply.github.com>
2022-08-17 00:57:07 +0530

    Add font weights

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -60,7 +60,7 @@ export default class MyDocument extends Document {
       <Html lang="en">
         <Head>
           <link
-            href="https://fonts.googleapis.com/css2?family=Inter:wght@700&display=swap"
+            href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600&display=swap"
             rel="stylesheet"
           />
         </Head>
