1c7a1a775e28428efff9618328d8f5f453c99d62
haZya <63403456+haZya@users.noreply.github.com>
2022-08-22 15:15:54 +0530

    Center text

--- components/shared/Footer.tsx
+++ components/shared/Footer.tsx
@@ -53,7 +53,7 @@ const Footer = () => {
           </ul>
         </div>
       </div>
-      <div className="flex justify-center items-center w-full h-10 bg-neutral-200 border-t border-secondary-main">
+      <div className="flex justify-center items-center w-full h-10 text-center bg-neutral-200 border-t border-secondary-main">
         <Typography className="text-xs font-medium">
           Copyright © 2022{' '}
           <Link href="/">
