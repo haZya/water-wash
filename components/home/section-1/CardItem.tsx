9ddd431636e0d1766061a14a0df0fbc592d9650e
haZya <63403456+haZya@users.noreply.github.com>
2022-08-22 20:21:02 +0530

    Add section-1 background
    + Add wave svg

--- components/home/section-1/CardItem.tsx
+++ components/home/section-1/CardItem.tsx
@@ -1,17 +1,22 @@
-import { Box, Typography } from '@mui/material';
+import { Box, Typography, useMediaQuery, useTheme } from '@mui/material';
 import clsx from 'clsx';
+import useInView from 'hooks/useInView';
 import { sanitize } from 'lib/dompurify';
 import { ISection1Item } from 'models/home';
 import styles from './CardItem.module.css';
 
 interface IProps extends ISection1Item {
   index: number;
-  inView: boolean;
 }
 
-const CardItem = ({ index, inView, icon, title, content }: IProps) => {
+const CardItem = ({ index, icon, title, content }: IProps) => {
+  const theme = useTheme();
+  const xsDown = useMediaQuery(theme.breakpoints.down('xs'));
+  const [ref, inView] = useInView<HTMLDivElement>(xsDown ? '-10px' : '-80px');
+
   return (
     <Box
+      ref={ref}
       className={clsx('translate-y-16 opacity-0', inView && styles.slideUp)}
       sx={{
         animationDelay: `${index * 0.3}s`,
@@ -24,13 +29,13 @@ const CardItem = ({ index, inView, icon, title, content }: IProps) => {
         )}
       >
         <div
-          className="flex-center text-white bg-secondary-main group-hover:bg-primary-main transition-color duration-700 rounded-full w-20 h-20 mx-auto"
+          className="flex-center text-white bg-primary-100 group-hover:bg-secondary-400 transition-color duration-700 rounded-full w-20 h-20 mx-auto"
           dangerouslySetInnerHTML={{ __html: sanitize(icon) }}
         />
-        <Typography className="text-center text-lg font-medium" variant="h3">
+        <Typography className="text-center text-lg font-medium select-none" variant="h3">
           {title}
         </Typography>
-        <Typography className="text-center text-xs">{content}</Typography>
+        <Typography className="text-center text-xs select-none">{content}</Typography>
       </div>
     </Box>
   );
--- components/home/section-1/Section1.tsx
+++ components/home/section-1/Section1.tsx
@@ -1,35 +1,38 @@
 import { Typography } from '@mui/material';
-import { useInView } from 'hooks';
 import { sanitize } from 'lib/dompurify';
 import { RootState } from 'lib/redux';
 import { useSelector } from 'react-redux';
 import CardItem from './CardItem';
+import Wave from './Wave';
 
 const Section1 = () => {
   const { title, subtitle, items } = useSelector(({ home }: RootState) => home.content.section1);
-  const [ref, inView] = useInView<HTMLDivElement>('-80px');
 
   return (
-    <section aria-labelledby="section-1-title" className="bg-sky-100 py-16">
-      <div className="container mx-auto">
-        <header className="flex flex-col items-center">
-          <Typography
-            className="text-4xl text-center font-medium leading-tight"
-            id="section-1-title"
-            variant="h2"
-          >
-            <div dangerouslySetInnerHTML={{ __html: sanitize(title) }} />
-          </Typography>
-          <Typography className="text-center text-base my-6" variant="subtitle1">
-            <div dangerouslySetInnerHTML={{ __html: sanitize(subtitle) }} />
-          </Typography>
-        </header>
-        <div ref={ref} className="grid grid-cols-3 gap-12 mt-4">
-          {items.map((item, i) => (
-            <CardItem key={item.title} index={i} inView={inView} {...item} />
-          ))}
+    <section aria-labelledby="section-1-title" className="relative">
+      <div className="bg-gradient-to-r from-primary-100 to-secondary-100 py-16 px-2">
+        <div className="container mx-auto">
+          <header className="flex flex-col items-center">
+            <Typography
+              className="text-3xl sm:text-4xl text-center font-bold leading-tight"
+              id="section-1-title"
+              variant="h2"
+              color="text.secondary"
+            >
+              <div dangerouslySetInnerHTML={{ __html: sanitize(title) }} />
+            </Typography>
+            <Typography className="text-center text-base my-6" variant="subtitle1">
+              <div dangerouslySetInnerHTML={{ __html: sanitize(subtitle) }} />
+            </Typography>
+          </header>
+          <div className="grid grid-cols-1 xs:grid-cols-3 gap-6 xs:gap-4 sm:gap-8 lg:gap-12 mt-4">
+            {items.map((item, i) => (
+              <CardItem key={item.title} index={i} {...item} />
+            ))}
+          </div>
         </div>
       </div>
+      <Wave />
     </section>
   );
 };
