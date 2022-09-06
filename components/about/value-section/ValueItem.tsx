25e10147acf5961d2d71317b4dccb7faf5805fce
haZya <63403456+haZya@users.noreply.github.com>
2022-09-06 06:51:58 +0530

    Add tilt and scale

--- components/about/value-section/ValueItem.tsx
+++ components/about/value-section/ValueItem.tsx
@@ -5,6 +5,7 @@ import { useInView } from 'hooks';
 import { sanitize } from 'lib/dompurify';
 import { IValueSectionItem } from 'models/about';
 import { useEffect, useState } from 'react';
+import Tilt from 'react-parallax-tilt';
 import styles from './ValueItem.module.css';
 
 interface IProps extends IValueSectionItem {
@@ -28,49 +29,64 @@ function ValueItem({ index, badge, title, content }: IProps) {
   }, [animate]);
 
   return (
-    <Box
-      ref={ref}
-      className={clsx(
-        'opacity-0 invisible rounded-3xl shadow-md overflow-hidden',
-        animate && styles.slideUp
-      )}
-      onAnimationStart={handleAnimationStart}
-      onAnimationEnd={handleAnimationEnd}
-      sx={{
-        animationDelay: `${(index + 1) * 0.15}s`,
-      }}
-    >
-      <div className="bg-black py-4">
-        <div className="max-w-64 mx-auto">
-          <Image src={badge} alt="Badge" />
-        </div>
-      </div>
-      <div className={clsx('flex flex-col max-w-sm md:max-w-none p-6 sm:py-8 sm:px-10 space-y-8')}>
-        <Typography
-          className="relative text-4xl font-normal"
-          variant="h2"
+    <div className={clsx(styles.wrapper)}>
+      <Tilt
+        className={clsx('w-full h-full')}
+        scale={1.06}
+        tiltMaxAngleX={15}
+        tiltAxis="y"
+        glareEnable
+        glarePosition="all"
+        glareMaxOpacity={0.3}
+        tiltAngleYInitial={index === 0 ? -15 : index === 2 ? 15 : 0}
+      >
+        <Box
+          ref={ref}
+          className={clsx(
+            'opacity-0 invisible rounded-3xl shadow-md overflow-hidden',
+            animate && styles.slideUp
+          )}
+          onAnimationStart={handleAnimationStart}
+          onAnimationEnd={handleAnimationEnd}
           sx={{
-            '&::after': {
-              content: '""',
-              position: 'absolute',
-              backgroundColor: 'currentColor',
-              bottom: '-6px',
-              left: 0,
-              width: '44px',
-              height: '4px',
-              borderRadius: '10px',
-            },
+            animationDelay: `${(index + 1) * 0.15}s`,
           }}
         >
-          {title}
-        </Typography>
-        <Typography
-          className="text-base text-justify font-medium space-y-4"
-          color="text.secondary"
-          dangerouslySetInnerHTML={{ __html: sanitize(content) }}
-        />
-      </div>
-    </Box>
+          <div className="bg-black py-4">
+            <div className="max-w-64 mx-auto">
+              <Image src={badge} alt="Badge" />
+            </div>
+          </div>
+          <div
+            className={clsx('flex flex-col max-w-sm md:max-w-none p-6 sm:py-8 sm:px-10 space-y-8')}
+          >
+            <Typography
+              className="relative text-4xl font-normal"
+              variant="h2"
+              sx={{
+                '&::after': {
+                  content: '""',
+                  position: 'absolute',
+                  backgroundColor: 'currentColor',
+                  bottom: '-6px',
+                  left: 0,
+                  width: '44px',
+                  height: '4px',
+                  borderRadius: '10px',
+                },
+              }}
+            >
+              {title}
+            </Typography>
+            <Typography
+              className="text-base text-justify font-medium space-y-4"
+              color="text.secondary"
+              dangerouslySetInnerHTML={{ __html: sanitize(content) }}
+            />
+          </div>
+        </Box>
+      </Tilt>
+    </div>
   );
 }
 
--- components/about/value-section/ValueSection.tsx
+++ components/about/value-section/ValueSection.tsx
@@ -7,21 +7,10 @@ const ValueSection = () => {
 
   return (
     <section aria-label="Our Values">
-      <div className="container mx-auto pt-20 sm:pt-40">
-        <div className="h-[96rem]" />
-        <div
-          // variants={container}
-          // initial="hidden"
-          // animate="show"
-          className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-y-6 lg:gap-y-0 md:gap-x-6 items-center"
-        >
+      <div className="container mx-auto py-8 sm:py-16">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-y-6 lg:gap-y-0 md:gap-x-6 items-center">
           {items.map((b, i) => (
-            <div
-              key={b.title}
-              // variants={item}
-            >
-              <ValueItem index={i} {...b} />
-            </div>
+            <ValueItem key={b.title} index={i} {...b} />
           ))}
         </div>
       </div>
