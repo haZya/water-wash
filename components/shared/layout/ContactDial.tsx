2e8ddc72066025eb0e090664b79641998ac7d860
haZya <63403456+haZya@users.noreply.github.com>
2022-09-25 22:45:03 +0530

    Fix bottom on breakpoint

--- components/shared/layout/ContactDial.tsx
+++ components/shared/layout/ContactDial.tsx
@@ -1,5 +1,13 @@
 import { Close } from '@mui/icons-material';
-import { lighten, Link, SpeedDial, SpeedDialAction, SpeedDialIcon } from '@mui/material';
+import {
+  lighten,
+  Link,
+  SpeedDial,
+  SpeedDialAction,
+  SpeedDialIcon,
+  useMediaQuery,
+  useTheme,
+} from '@mui/material';
 import { usePageBottom } from 'hooks';
 import { sanitize } from 'lib/dompurify';
 import { RootState } from 'lib/redux';
@@ -13,6 +21,8 @@ const ContactDial = () => {
       contactDial: { icon, color, actions },
     },
   } = useSelector(({ shared }: RootState) => shared.layout);
+  const theme = useTheme();
+  const smDown = useMediaQuery(theme.breakpoints.down('sm'));
 
   const scrollFooter = usePageBottom(1);
   const ref = useRef<HTMLElement>(null);
@@ -30,19 +40,14 @@ const ContactDial = () => {
         />
       }
       sx={{
-        bottom: (theme) =>
-          scrollFooter
-            ? footerHeight - (ref.current?.clientHeight ?? 0)
-            : theme.breakpoints.down('sm')
-            ? 18
-            : 44,
+        bottom: scrollFooter ? footerHeight - (ref.current?.clientHeight ?? 0) : smDown ? 18 : 44,
         transition: 'bottom .3s',
         '& .MuiButtonBase-root': {
           boxShadow:
             'inset 10px 10px 10px rgba(0, 0, 0, 0.05), 15px 25px 10px rgba(0, 0, 0, 0.05), 15px 20px 20px rgba(0, 0, 0, 0.05), inset -10px -10px 15px rgba(255, 255, 255, 09)',
-          bgcolor: (theme) => lighten(theme.palette[color][100], 0.8),
+          bgcolor: lighten(theme.palette[color][100], 0.8),
           '&:hover': {
-            bgcolor: (theme) => lighten(theme.palette[color][100], 0.5),
+            bgcolor: lighten(theme.palette[color][100], 0.5),
           },
           '&.MuiFab-sizeLarge': {
             width: 60,
@@ -67,7 +72,7 @@ const ContactDial = () => {
           sx={{
             '& .MuiSpeedDialAction-staticTooltipLabel': {
               color: 'white',
-              bgcolor: (theme) => theme.palette[color][900],
+              bgcolor: theme.palette[color][900],
             },
           }}
         />
